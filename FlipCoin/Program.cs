3079fcac1f9f05bd70cce06e763695a4786f602a
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-27 19:25:14 +0530

    [Wasim] Add: FlipCoin

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int head = 0, tail = 0;
+            while (head < 21 || tail < 21 || (head - tail) < 2)
+            {
+                if (random.Next(0, 2) == 0)
+                    head++;
+                else
+                    tail++;
+            }
+            if (head > tail)
+                Console.WriteLine("Head Won by " + (head - tail));
+            else
+                Console.WriteLine("Tail Won by " + (tail - head));
+        }
+    }
+}
